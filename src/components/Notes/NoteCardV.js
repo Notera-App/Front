d705ebf613bc0db429f0b2a86dbd7b1d0f541fda
AndresPinzon14 <andrespinzon1414@gmail.com>
2022-04-24 01:12:29 -0500

    color pick

--- src/components/Notes/NoteCardV.js
+++ src/components/Notes/NoteCardV.js
@@ -1,16 +1,37 @@
-import { Accordion, Button, Card, Col, Row } from "react-bootstrap";
+import { Form, Button, Card, Col, Row } from "react-bootstrap";
 import { BiSitemap } from "react-icons/bi";
 import { MdOutlineFlipCameraAndroid } from "react-icons/md";
 import { AiTwotoneSound } from "react-icons/ai";
 import { AiFillQuestionCircle } from "react-icons/ai";
 import { Cont } from "./Cont";
 import { TextToVoice } from "../elements/TextToVoice";
+import { useState } from "react";
+import { getValue } from "@testing-library/user-event/dist/utils";
 
 export const NoteCardV = ({ concept }) => {
+  const [color, setColor] = useState("f4d06f");
+
+  const handleChangeColor = (val) => {   
+    console.log(val)
+    setColor(val);
+  };
+
+
+
   console.log(concept);
   return (
-    <div className="Card-Container">
-      <Card className="Note-Card">
+    <div className="Card-Container">     
+      <Form.Control 
+        className="color-pick-box"
+        type="color"
+        id="exampleColorInput"
+        defaultValue="#f4d06f"
+        title="Elige el color de la Nota"
+        onChange={(e) => handleChangeColor(e.target.value)}
+      />
+      <Card className="Note-Card" style={{
+              backgroundColor: {color},             
+            }}>
         <Card.Body>
           <Card.Title className="text-center">
             <h2>{concept.title}</h2>
