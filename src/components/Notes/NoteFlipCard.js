c1cf753f6df20a3dea9ac099b65484d53e3446b2
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-04-23 22:22:24 -0500

    react-create-app on Heroku

--- src/components/Notes/NoteFlipCard.js
+++ src/components/Notes/NoteFlipCard.js
@@ -1,48 +1,48 @@
-import ReactCardFlip from "react-card-flip";
-import { Button, Card, Col, Row } from "react-bootstrap";
-import { BiSitemap } from "react-icons/bi";
-import { MdOutlineFlipCameraAndroid } from "react-icons/md";
-import { AiTwotoneSound } from "react-icons/ai";
-import { AiFillQuestionCircle } from "react-icons/ai";
-import { Cont } from "./Cont";
-import { useState } from "react";
+// import ReactCardFlip from "react-card-flip";
+// import { Button, Card, Col, Row } from "react-bootstrap";
+// import { BiSitemap } from "react-icons/bi";
+// import { MdOutlineFlipCameraAndroid } from "react-icons/md";
+// import { AiTwotoneSound } from "react-icons/ai";
+// import { AiFillQuestionCircle } from "react-icons/ai";
+// import { Cont } from "./Cont";
+// import { useState } from "react";
 
-export const NoteFlipCard = ({ concept }) => {
-  const [isFlipped, setFlip] = useState(false);
+// export const NoteFlipCard = ({ concept }) => {
+//   const [isFlipped, setFlip] = useState(false);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setFlip(!isFlipped);
-  };
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     setFlip(!isFlipped);
+//   };
 
-  return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-     {/* Parte Frontal de la Card */}
-      <button className="m-0 p-0" onClick={handleClick}><Card className="Note-Card">
-        <Card.Body>
-          <Card.Title className="text-center">{concept.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {concept.description}
-          </Card.Subtitle>
-          <Card.Text>
-            <Cont content={concept.content} />
-          </Card.Text>              
-        </Card.Body>
-      </Card>
-      </button>
-      {/* Parte Trasera de la Card */}
-      <button className="m-0 p-0" onClick={handleClick}><Card className="Note-Card">
-        <Card.Body>
-          <Card.Title className="text-center">{concept.title} Back</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {concept.description}
-          </Card.Subtitle>
-          <Card.Text>
-            <Cont content={concept.content} />
-          </Card.Text>              
-        </Card.Body>
-      </Card>
-      </button>
-    </ReactCardFlip>
-  );
-};
+//   return (
+//     <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+//      {/* Parte Frontal de la Card */}
+//       <button className="m-0 p-0" onClick={handleClick}><Card className="Note-Card">
+//         <Card.Body>
+//           <Card.Title className="text-center">{concept.title}</Card.Title>
+//           <Card.Subtitle className="mb-2 text-muted">
+//             {concept.description}
+//           </Card.Subtitle>
+//           <Card.Text>
+//             <Cont content={concept.content} />
+//           </Card.Text>              
+//         </Card.Body>
+//       </Card>
+//       </button>
+//       {/* Parte Trasera de la Card */}
+//       <button className="m-0 p-0" onClick={handleClick}><Card className="Note-Card">
+//         <Card.Body>
+//           <Card.Title className="text-center">{concept.title} Back</Card.Title>
+//           <Card.Subtitle className="mb-2 text-muted">
+//             {concept.description}
+//           </Card.Subtitle>
+//           <Card.Text>
+//             <Cont content={concept.content} />
+//           </Card.Text>              
+//         </Card.Body>
+//       </Card>
+//       </button>
+//     </ReactCardFlip>
+//   );
+// };
