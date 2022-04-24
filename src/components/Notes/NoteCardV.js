ffbf7127a04aadcb935dab54e991e30f9f4d6a49
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-04-23 23:19:32 -0500

    Note corrected

--- src/components/Notes/NoteCardV.js
+++ src/components/Notes/NoteCardV.js
@@ -10,51 +10,33 @@ export const NoteCardV = ({ concept }) => {
   console.log(concept);
   return (
     <div className="Card-Container">
-    <Card className="Note-Card">
-      <Card.Body>
-        <Card.Title className="text-center">
-          <h2>{concept.title}</h2>
-        </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          {concept.description}
-        </Card.Subtitle>
-        <Card.Text>
-          <Cont content={concept.content} />
-        </Card.Text>
-        <Row>
-          <Col className="m-auto" sm>
-            <Button className="icon-button p-auto" title="Mapa Conceptual">
-              <BiSitemap />
-            </Button>
-            <Button className="icon-button" title="Voltear Card">
-              <MdOutlineFlipCameraAndroid />
-            </Button>
-            <TextToVoice title={concept.title} content={concept.content} />
-            <Button className="icon-button" title="Cuestionario">
-              <AiFillQuestionCircle />
-            </Button>
-          </Col>
-        </Row>
-        {/* <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link> */}
-      </Card.Body>
-    </Card>
-    <Row>
-    <Col className="m-auto" sm>
-      <Button className="icon-button p-auto" title="Mapa Conceptual">
-        <BiSitemap />
-      </Button>
-      <Button className="icon-button" title="Voltear Card">
-        <MdOutlineFlipCameraAndroid />
-      </Button>
-      <Button className="icon-button" title="Convertir en Audio">
-        <AiTwotoneSound />
-      </Button>
-      <Button className="icon-button" title="Cuestionario">
-        <AiFillQuestionCircle />
-      </Button>
-    </Col>
-  </Row>
-  </div>
+      <Card className="Note-Card">
+        <Card.Body>
+          <Card.Title className="text-center">
+            <h2>{concept.title}</h2>
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {concept.description}
+          </Card.Subtitle>
+          <Card.Text>
+            <Cont content={concept.content} />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Row>
+        <Col className="m-auto" sm>
+          <Button className="icon-button p-auto" title="Mapa Conceptual">
+            <BiSitemap />
+          </Button>
+          <Button className="icon-button" title="Voltear Card">
+            <MdOutlineFlipCameraAndroid />
+          </Button>
+          <TextToVoice title={concept.title} content={concept.content} />
+          <Button className="icon-button" title="Cuestionario">
+            <AiFillQuestionCircle />
+          </Button>
+        </Col>
+      </Row>
+    </div>
   );
 };
